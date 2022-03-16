9cf582fde31c2d931b5d6f6318346e89c5dd8542
Andrej Byrtus <a.byrtus99@gmail.com>
2022-03-16 13:36:47 +0100

    added login id response

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Client } = require('pg');
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 const client = new Client({
@@ -12,14 +13,31 @@ const client = new Client({
 client.connect();
 
 app.use(express.static('images'));
+app.use(express.json());
 
-app.get('/register', (req, res) => {
-    console.log(req.body);
+app.get('/', (req, res) => {
+    res.send("Hello from server.");
+});
+
+app.post('/register', (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
-    client.query(`INSERT INTO public.User(username, password) VALUES (${username}, ${password})`, (err, query_res) => {
-        res.status(200);
-        res.json({"username": username, "password": password});
+    client.query(`INSERT INTO "User" (username, password) VALUES ('${username}', '${password}')`, (err, query_res) => {
+        if (err) {
+            res.status(500);
+            res.json({"error": err});
+        } 
+        else {
+            client.query(`SELECT id FROM "User" WHERE "User".username = '${username}'`, (err2, second_query_res) => {
+                if (err2) {
+                    res.status(500);
+                    res.json({"error": err});
+                    return;
+                }
+                res.status(200);
+                res.json({"id": second_query_res.rows[0].id});
+            });
+        }
     });
     console.log(`register request user=${username} password=${password}`);
 })
