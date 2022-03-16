5b5f798afae2376b9c365fd59cfc22f995bad445
Andrej Byrtus <a.byrtus99@gmail.com>
2022-03-16 12:57:13 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { Client } = require('pg');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const client = new Client({
+    connectionString: process.env.DB_URL || require('./local_keys/connection_key.json')["connectionString"],
+    ssl: false,
+});
+client.connect();
+
+app.use(express.static('images'));
+
+app.get('/register', (req, res) => {
+    console.log(req.body);
+    let username = req.body.username;
+    let password = req.body.password;
+    client.query(`INSERT INTO public.User(username, password) VALUES (${username}, ${password})`, (err, query_res) => {
+
+    });
+    console.log("health request");
+    res.send("a");
+})
+
+app.get('/test', (req, res) => {
+    console.log(req);
+    res.send("AAAAAAA");
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
